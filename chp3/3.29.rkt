65e6967ffa018d2ae6d8ba9e0a3dbb131c1287ad
Amin <ait.amin93@gmail.com>
2020-03-30 17:35:39 +0200

    Fix or-gate solution
    Apparently I skipped some logic classes, since I thought that
    A OR B <==> NOT(A AND B) :'D

--- chp3/3.29.rkt
+++ chp3/3.29.rkt
@@ -1,13 +1,18 @@
 #lang sicp
 
-(#%require (file "../util.rkt"))
+(#%require (file "../util.rkt") 
+           (file "digital-circuits.rkt"))
 
 ;ex. 3.29
-; or-gate-delay = and-gate-delay + inverter-delay
+; or-gate-delay = and-gate-delay + 3*inverter-delay
 
-(define (or-gate a1 a2 output)
-    (let ((c (make-wire)))
-        (and-gate a1 a2 c)
-        (inverter c output)
+(define (or-gate1 a1 a2 output)
+    (let ((not-a1 (make-wire))
+          (not-a2 (make-wire))
+          (and-not-a1-not-a2 (make-wire)))
+        (inverter a1 not-a1)
+        (inverter a2 not-a2)
+        (and-gate not-a1 not-a2 and-not-a1-not-a2)
+        (inverter and-not-a1-not-a2 output)
         'ok))
     
